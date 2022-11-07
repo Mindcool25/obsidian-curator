8678a64f00efe963b4fd0583812fa6a7146180f5
mindcool24 <Mindcool24@jsociety.xyz>
2022-11-07 12:12:25 -0700

    Added support for nested folders

--- rootgen.py
+++ rootgen.py
@@ -20,6 +20,7 @@ def main():
         print(f"Path:\n{page.page_path}\nContent:\n{page.content}")
     write_page(pages)
 
+
 def gen_root(pages):
     for page in pages:
         if page.page_path != "root.md":
@@ -29,11 +30,17 @@ def gen_root(pages):
 def gen_page(pages):
     for page in pages:
         if page.page_path != "root.md":
-            files = os.listdir(f"{page.page_name}")
+            files = os.listdir(page.page_name)
             for file in files:
-                if not os.path.isdir(file):
+                file_path = f"{page.page_name}/{file}"
+                if os.path.isfile(file_path):
                     file_name = file.split('.')[0]
                     page.content += f"## [[{file_name}]]\n"
+                elif os.path.isdir(file_path):
+                    new_page = Page(f"{file_path}", f"# {file}\n")
+                    new_page.page_path = f"./{file_path}.md"
+                    pages.append(new_page)
+    
     return pages
 
 def write_page(pages):
